384c3aaf5abc10ea014febe87014a74d5efdd63e
Nick Pascetti <npascetti@cnm.edu>
2017-12-10 22:38:09 -0700

    started angular tour of heroes demo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './heroes/hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'KumDumpStarr' },
+    { id: 12, name: 'Narc' },
+    { id: 13, name: 'Grumpy Gussy' },
+    { id: 14, name: 'Awesome Possum' },
+    { id: 15, name: 'Tim Curry' },
+    { id: 16, name: 'Salt Peter' },
+    { id: 17, name: 'Nik 86' },
+    { id: 18, name: 'Cornholio' },
+    { id: 19, name: 'Mamma Mia' },
+    { id: 20, name: 'The Blur' }
+];
